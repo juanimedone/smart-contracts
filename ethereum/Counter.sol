f1cc2f9585aa2050588e2c7e37cb764f6155f742
juanimedone <juanimedone@gmail.com>
2025-04-27 13:33:11 -0300

    Add external library import

--- ethereum/Counter.sol
+++ ethereum/Counter.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// import "@openzeppelin/contracts/utils/math/SafeMath.sol";    the contracts to import need to be installed
 contract Counter {
+    // using SafeMath for uint256;
+
     uint256 public count = 0;
 
     function increment() public {
